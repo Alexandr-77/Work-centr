d7f1badf1047d226237af2dab48c5c637ce5dbd7
Sasha-Tes <salamon-77@mail.ru>
2021-04-14 13:09:17 +0300

    Готовый проект

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function yur() {
+    document.getElementById("a").style.display = "block"
+    document.getElementById("b").style.display = "none"
+ };
+ 
+ 
+ function fiz() {
+    document.getElementById("b").style.display = "block"
+    document.getElementById("a").style.display = "none"
+ };
+ 
